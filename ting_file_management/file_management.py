6aa940c7bc89ebbdf655d0a6a63924b9580902e4
Mauricio-OC <murisc0@hotmail.com>
2023-09-03 23:24:57 -0300

    req 01

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,13 +1,14 @@
 import sys
 
+
 def txt_importer(path_file):
     try:
         if not path_file.endswith(".txt"):
             raise ValueError("Formato inválido")
-        
+
         with open(path_file, 'r', encoding='utf-8') as file:
             lines = file.read().split('\n')
-        
+
         return lines
     except FileNotFoundError:
         error_message = f"Arquivo {path_file} não encontrado"
@@ -16,4 +17,3 @@ def txt_importer(path_file):
     except ValueError as e:
         print(e, file=sys.stderr)
         return []
-
